e68dcee8ab07a5e12fe485bc3c845452c7945989
Annina14 <annalisa.bovone@gmail.com>
2016-11-14 16:43:40 +0100

    ItemRicercaActivityFragmentList

--- app/src/main/java/com/example/ale/myapplicatio/RicercaActivityFragmentList.java
+++ app/src/main/java/com/example/ale/myapplicatio/RicercaActivityFragmentList.java
@@ -39,7 +39,11 @@ public class RicercaActivityFragmentList extends Fragment implements AdapterView
     private Button altro;
 
     public RicercaActivityFragmentList() {
-
+        int barlocco = 100;
+        String paola = "brutta";
+        if(paola.equals("ciao")){
+            barlocco = 50;
+        }
     }
 
     /*public static RicercaActivityFragmentList newInstance(String param1, String param2) {
