b3d5fa65189bcf157ff3c1b07bc7ff0203657b0f
pani94 <ale_94_10@hotmail.it>
2016-11-18 15:22:42 +0100

    romualdo

--- app/src/main/java/com/example/ale/myapplicatio/GestioneViaggioAttivitaTabTutte.java
+++ app/src/main/java/com/example/ale/myapplicatio/GestioneViaggioAttivitaTabTutte.java
@@ -9,11 +9,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class GestioneViaggioAttivitaTabTutte extends Fragment {
+import java.util.ArrayList;
+
+public class GestioneViaggioAttivitaTabTutte extends Fragment implements AdapterView.OnItemClickListener{
+
+    private ArrayList<Attivita> attivitas;
+    private ListView itemListView;
+    private String nomeViaggio;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            nomeViaggio = getArguments().getString("attivita_nomeviaggio");
+
+        }
+
+
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_gestione_viaggio_attivita, container, false);
+        attivitas = new ArrayList<Attivita>();
+        DataBase db = new DataBase(getActivity());
+        attivitas = db.getAttivita(nomeViaggio);
+        itemListView = (ListView) rootView.findViewById(R.id.fragment_gestione_viaggio_attivita_lista);
+        ItemAdapterAttivita adapter = new ItemAdapterAttivita(getActivity(),attivitas);
+        adapter.notifyDataSetChanged();
+        itemListView.setAdapter(adapter);
+        itemListView.setOnItemClickListener(this);
         return rootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
