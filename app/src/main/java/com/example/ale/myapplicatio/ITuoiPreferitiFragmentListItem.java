8a1da506b7d5d958388a6e7b61ce8d4de590181d
Paola <silvestre4@hotmail.it>
2016-11-27 17:09:49 +0100

    preferiti star

--- app/src/main/java/com/example/ale/myapplicatio/ITuoiPreferitiFragmentListItem.java
+++ app/src/main/java/com/example/ale/myapplicatio/ITuoiPreferitiFragmentListItem.java
@@ -3,6 +3,8 @@ package com.example.ale.myapplicatio;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -69,7 +71,7 @@ public class ITuoiPreferitiFragmentListItem extends Fragment {
         View view = inflater.inflate(R.layout.fragment_ituoi_preferiti_fragment_list_item, container, false);
         preferiti_titolo = (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_titolo);
         preferiti_foto = (ImageView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_immagine);
-        preferiti_orario = (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_orario);
+        preferiti_orario = (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_list_item_orario);
         preferiti_link= (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_link);
         preferiti_telefono = (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_telefono);
         preferiti_indirizzo = (TextView) view.findViewById(R.id.fragment_ituoi_preferiti_fragment_list_item_indirizzo);
@@ -84,6 +86,9 @@ public class ITuoiPreferitiFragmentListItem extends Fragment {
         database = new DataBase(getActivity());
         arrayListViaggi = database.getViaggi();
         preferiti_bottoneaggiungi.setOnClickListener(buttonListener);
+
+        preferiti_titolo.setTypeface(Typeface.DEFAULT_BOLD);
+        preferiti_titolo.setShadowLayer(5,0,0, Color.BLACK);
 
         String photo_reference_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + preferiti_foto_get + "&sensor=false&key=AIzaSyCG-pKhY5jLgcDTJZSaTUd3ufgvtcJ9NwQ";
         new LoadImageTask().execute(photo_reference_url);
